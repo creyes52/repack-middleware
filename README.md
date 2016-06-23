efc8aea7ca994c2cbf918b7a2342f5b098be8ea5
Cesar <creyes@xik.gt>
2016-06-23 10:08:03 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+
+# Repack middleware
+## Reac, with webpack middleware and hot module replacement
+Simplify installatin of webpack-middleware with hot module replacement in an express-like application,
+if using React, it will also add a res.renderReact(component, vars) to generate react html on the server
+that can be used to iplement isomorphic react.
+
+## Installation & usage
+
+Frist, install the npm module
+```
+npm install --save-dev repack-middleware
+```
+
+Next, create your webpack config
+Example:
+
+./webpack.config.js
+```
+module.exports = {
+        entry: "./components/main.js",
+        output: {
+                //path: "./public/build",
+                filename: "app.bundle.js",
+                publicPath: "/build/"
+        }
+}
+
+```
+
+Now add the middleware into your server.
+```
+var repackMiddleware = require('repack-middleware');
+
+
+app.use(repackMiddleware({
+	configFile: __dirname + "/webpack.config.js"
+});
+
+```
+
+With the configuration above, the middlware will serve the bundled resource in the route **/build/app.bundle.js**  
+Add it to your html:  
+
+```
+<script type="text/javascript" src="/build/app.bundle.js"></script>
+```
+
+Now, the module will have **module.hot** as an object.
+Example of how to make the module aware of changes.
+```
+var render = function() {
+    document.getElementById("main").innerHTML = "Hola mundo " + Date.now();
+}
+render();
+
+if ( module.hot ) { 
+    module.hot.accept(function() {
+        render();
+    }); 
+}
+```
+
+
+## Documentation
+
+### Config
+Configuration options are passed to the middleware when it is created.
+
+ * **configFile** - The path to your webpack.config.js
+
+
+
---
+++ index.js
@@ -0,0 +1,135 @@
+var React          = require('react');
+var ReactDOMServer = require('react-dom/server');
+var components = {};
+var _                    = require('lodash');
+var webpack              = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var path                 = require('path');
+require('babel-register');
+
+module.exports = (function(options) {
+    options               = options || {};
+    var webpackConfigFile = options.configFile || "";
+    var componentsPath    = options.componentsPath || "";
+    var isProd            = options.productionMode ? options.productionMode : process.env.NODE_ENV == 'prod';
+    
+    var webpackConfig     = require(webpackConfigFile);
+
+
+    var renderFn = function(componentName, vars) {
+        try {
+        var component = components[componentName];
+        if ( component == undefined ) {
+            var compPath  = path.join(componentsPath, `${componentName}.jsx`);
+            var compCtor  = require( compPath ).default;
+            component = React.createFactory( compCtor );
+            if ( isProd ) {
+                components[componentName] = component; 
+            }
+        }
+
+        var element = component(vars);
+        return ReactDOMServer.renderToString( element );
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    var renderMiddleware = function renderMiddleware(req, res, next) {
+        res.renderReact = function(component, vars) {
+            var html = "";
+            html += `<div id='main'>${renderFn( component, vars)}</div>`;
+            html += `<script type='text/javascript'>var INIT = ${JSON.stringify(vars)}</script>`;
+            res.locals.reactHtml = html;
+            //return res.view();
+			return html;
+        }
+        next();
+    }
+
+    var nullMiddleware = function nullMiddleware(req, res, next) {
+        next();
+    }
+
+    /**
+    * Configuring webpack middleware
+    * This middleware, and the HMR (hot module replacement) are only used in develpment
+    *
+    */
+    
+    //
+    // ======== reconfigure configuration =======
+    //
+    var config = _.defaultsDeep({}, webpackConfig, {
+        output: {
+            path: "/",
+            publicPath: "/build/" // used to prefix json updates
+        },
+        devtool: 'source-map',
+        plugins: []
+    });
+
+    //
+    //  ========== HMR configuration ======
+    //
+    if ( typeof config.entry == 'string' ) {
+        config.entry = [ config.entry ];
+    }
+	var hmrPath = __dirname + '/node_modules/webpack-hot-middleware/client.js';
+	//hmrPath = "webpack-hot-middleware/client";
+    var addHmr = function( entry ) { entry.unshift( hmrPath ); }
+    if ( typeof config.entry.length == 'number' ) {
+        addHmr( config.entry );
+    }
+    else if ( typeof config.entry == 'object' ) {
+        _.values( config.entry ).forEach( addHmr );
+    } 
+
+    config.plugins.unshift(
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    );
+
+    //
+    // ======= webpack middleware and hmr middleware ====
+    //
+    
+    var compiler = webpack( config );
+    var wp       = webpackDevMiddleware( compiler, {
+        publicPath: config.output.publicPath , // public path where bundle and json is actually served
+        stats: {
+            colors: true,
+        },
+        noInfo: true,
+        watchOptions:{
+            poll: true
+        }
+    });
+
+    var whmr = webpackHotMiddleware(compiler);
+
+	var repackMiddleware = function repackMiddleware(req, res, next) {
+		var stack = [ wp, whmr, renderMiddleware];
+		var pos   = 0;
+
+		if ( isProd ) {
+			// we won't call webpack-middleware nor hot module replacement middleware in production
+			stack = [ renderMiddleware ];
+		}
+
+		var theNext = function() {
+			if ( pos < stack.length ) {
+				stack[pos++](req, res, theNext);
+			} else {
+				next();
+			}
+		};
+		theNext();
+	}
+
+	return repackMiddleware;
+});
+
+
