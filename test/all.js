58bb136d569f1fa7f58d7d2986605e1fd113861a
g3org3 <7jagjag@gmail.com>
2016-09-27 23:13:05 -0600

    remove helpers from tests

--- test/all.js
+++ test/all.js
@@ -1,7 +1,6 @@
 var assert  = require('chai').assert;
-var helpers = require('../helpers.js');
 var fs      = require('fs');
-var path        = require('path');
+var path    = require('path');
 var _       = require('lodash');
 
 var mkdir = function(path, cb) {
@@ -13,59 +12,6 @@ var mkdir = function(path, cb) {
 }
 
 describe('module', function() {
-
-	describe('helpers.js', function() {
-
-		var folderPath0= __dirname + "/../stub0";
-		var folderPath1= __dirname + "/../node_modules/stub2";
-		var folderPath2= __dirname + "/../stub1";
-		var folderPath3= __dirname + "/../../stub3-1234567"; // just an improbable name
-
-		before(function(cb) {
-			mkdir(folderPath0, function() {
-				mkdir(folderPath1, function() {
-					mkdir(folderPath2, function() {
-						mkdir(folderPath3, cb);
-					});
-				});
-			});
-		});
-
-		it('should find a folder in the same location', function() {
-			var pathstr = helpers.findModule('stub0');
-			assert(pathstr, "found nothing");
-			assert(pathstr.endsWith( path.join("repack-middleware", "stub0") ), "path is:" + pathstr);
-		});
-		it('should find a folder in the node modules', function() {
-			var pathstr = helpers.findModule('stub2');
-			//console.log(pathstr);
-			assert(pathstr, "found nothing");
-		});
-		it('should find a folder in the above', function() {
-			var pathstr = helpers.findModule('stub3-1234567');
-			//console.log(pathstr);
-			assert(pathstr, "found nothing");
-		});
-		it('should capture exceptions when folder does not exist', function() {
-			helpers.bases.unshift("/unexisting");
-
-			var path = helpers.findModule('unexisting/stub4');
-			assert.isUndefined(path, "path is defined");
-		});
-
-
-		after(function(cb) {
-			//return cb();
-			fs.rmdir(folderPath0, function() {
-				fs.rmdir(folderPath1, function() {
-					fs.rmdir(folderPath2, function() {
-						fs.rmdir(folderPath3, cb);
-					});
-				});
-			});
-		});
-
-	});
 
 	describe('reconfigure.js', function() {
 		var instance = null;
